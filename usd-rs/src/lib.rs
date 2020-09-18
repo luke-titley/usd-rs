da9c7fec73a51929febabb90c29790df64f941c9
Luke Titley <luke@titley.com>
2020-09-18 12:08:09 +0200

    Cleanup

--- usd-rs/src/lib.rs
+++ usd-rs/src/lib.rs
@@ -53,17 +53,6 @@ impl Stage {
     }
 }
 
-/*
-pub fn test ()
-{
-    let mut y = 2;
-    let mut z = 45;
-    let x: i32 = unsafe { cpp!([y as "int32_t", mut z as "int32_t"] -> i32 as "int32_t" {
-        z++;
-        return y + z;
-    })};
-}*/
-
 #[cfg(test)]
 mod tests {
     use super::*;
