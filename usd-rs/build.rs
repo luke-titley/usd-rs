c1cd90f5576b7522ee1dd081f23e7f5164c85240
Luke Titley <luke@titley.com>
2020-09-18 12:12:02 +0200

    Removed some dead code

--- usd-rs/build.rs
+++ usd-rs/build.rs
@@ -4,9 +4,6 @@ fn main() {
     println!("cargo:rustc-link-lib={}", usd_cpp::LIB);
     println!("cargo:rustc-link-search={}", usd_cpp::LIBS);
 
-    // The out directory of the build
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-
     cpp_build::Config::new()
         .include(usd_cpp::INCLUDE)
         .flag("-std=c++14")
