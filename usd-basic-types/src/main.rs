01faa086a129c60452462cd4da5002342e07ef5a
Luke Titley <luke@titley.com>
2020-09-21 10:55:16 +0200

    Have now added the int/float types

--- usd-basic-types/src/main.rs
+++ usd-basic-types/src/main.rs
@@ -6,82 +6,57 @@
 // Taken from :
 //      http://graphics.pixar.com/usd/docs/api/_usd__page__datatypes.html
 //
-//! bool		bool
-//! uchar		uint8_t	8 bit unsigned integer
-//! int		    int32_t	32 bit signed integer
-//! uint		uint32_t	32 bit unsigned integer
-//! int64		int64_t	64 bit signed integer
-//! uint64		uint64_t	64 bit unsigned integer
-//! half		GfHalf	16 bit floating point
-//! float		float	32 bit floating point
-//! double		double	64 bit floating point
-//! timecode	SdfTimeCode	double representing a resolvable time
-//! string		std::string	stl string
-//! token		TfToken	interned string with fast comparison and hashing
-//! asset		SdfAssetPath	represents a resolvable path to another asset
-//! matrix2d	GfMatrix2d	2x2 matrix of doubles
-//! matrix3d	GfMatrix3d	3x3 matrix of doubles
-//! matrix4d	GfMatrix4d	4x4 matrix of doubles
-//! quatd		GfQuatd	double-precision quaternion
-//! quatf		GfQuatf	single-precision quaternion
-//! quath		GfQuath	half-precision quaternion
-//! double2		GfVec2d	vector of 2 doubles
-//! float2		GfVec2f	vector of 2 floats
-//! half2		GfVec2h	vector of 2 half's
-//! int2		GfVec2i	vector of 2 ints
-//! double3		GfVec3d	vector of 3 doubles
-//! float3		GfVec3f	vector of 3 floats
-//! half3		GfVec3h	vector of 3 half's
-//! int3		GfVec3i	vector of 3 ints
-//! double4		GfVec4d	vector of 4 doubles
-//! float4		GfVec4f	vector of 4 floats
-//! half4		GfVec4h	vector of 4 half's
-//! int4		GfVec4i	vector of 4 ints
+
+/// bool		bool
+/// uchar		uint8_t	8 bit unsigned integer
+/// int		    int32_t	32 bit signed integer
+/// uint		uint32_t	32 bit unsigned integer
+/// int64		int64_t	64 bit signed integer
+/// uint64		uint64_t	64 bit unsigned integer
+/// half		GfHalf	16 bit floating point
+/// float		float	32 bit floating point
+/// double		double	64 bit floating point
+/// timecode	SdfTimeCode	double representing a resolvable time
+/// string		std::string	stl string
+/// token		TfToken	interned string with fast comparison and hashing
+/// asset		SdfAssetPath	represents a resolvable path to another asset
+/// matrix2d	GfMatrix2d	2x2 matrix of doubles
+/// matrix3d	GfMatrix3d	3x3 matrix of doubles
+/// matrix4d	GfMatrix4d	4x4 matrix of doubles
+/// quatd		GfQuatd	double-precision quaternion
+/// quatf		GfQuatf	single-precision quaternion
+/// quath		GfQuath	half-precision quaternion
+/// double2		GfVec2d	vector of 2 doubles
+/// float2		GfVec2f	vector of 2 floats
+/// half2		GfVec2h	vector of 2 half's
+/// int2		GfVec2i	vector of 2 ints
+/// double3		GfVec3d	vector of 3 doubles
+/// float3		GfVec3f	vector of 3 floats
+/// half3		GfVec3h	vector of 3 half's
+/// int3		GfVec3i	vector of 3 ints
+/// double4		GfVec4d	vector of 4 doubles
+/// float4		GfVec4f	vector of 4 floats
+/// half4		GfVec4h	vector of 4 half's
+/// int4		GfVec4i	vector of 4 ints
 
 //use half::f16;
-/*
-// TODO: Support arrays
-pub enum BasicValue {
-    Bool(bool),
-    UChar(u8),
-    Int(i32),
-    UInt(u32),
-    Int64(i64),
-    UInt64(u64),
-    Half(f16),
-    Float(f32),
-    Double(f64),
-    // TimeCode(sdf::TimeCode), // TODO: LT
-    String(std::string::String),
-    Token(tf::Token),
-    // Asset(sdf::AssetPath), // TODO: LT
-    Matrix2d([f64; 2 * 2]),
-    Matrix3d([f64; 3 * 3]),
-    Matrix4d([f64; 4 * 4]),
-    Quatd([f64; 4]),
-    Quatf([f32; 4]),
-    Quath([f16; 4]),
-    Double2([f64; 2]),
-    Float2([f32; 2]),
-    Half2([f16; 2]),
-    Int2([i32; 2]),
-    Double3([f64; 3]),
-    Float3([f32; 3]),
-    Half3([f16; 3]),
-    Int3([i32; 3]),
-    Double4([f64; 4]),
-    Float4([f32; 4]),
-    Half4([f16; 4]),
-    Int4([i32; 4]),
-}
-*/
 
 /// The basic types supported by USD.
 /// At the moment rust-cpp doesnt allow us to embed the cpp! macro inside of
 /// other macros. So we have to perform the code generation of the AsRef and
 /// From trait implementations as a manual step. This isn't such a big deal
 /// as basic types are rarely added or removed.
-const BASIC_TYPES: [[&str; 2]; 1] = [["bool", "bool"]];
+const BASIC_TYPES: [(&str, &str, Option<&str>); 8] = [
+    ("bool", "bool", None),
+    ("u8", "uint8_t", None),
+    ("i32", "int32_t", None),
+    ("u32", "uint32_t", None),
+    ("i64", "int64_t", None),
+    ("u64", "uint64_t", None),
+    //("f16", "GfHalf", None),
+    ("f32", "float", None),
+    ("f64", "double", None),
+];
 
 /// Generate the code needed to get/set the basic types, using a vt::Value.
 /// This is the code path that is used for getting the setting the values of
@@ -97,6 +72,8 @@ fn generate_basic_types() {
 use super::Value;
 use cpp::*;
 
+use half::f16; // Half is not a standard rust type
+
 cpp! {{{{
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wunused-parameter"
@@ -107,7 +84,7 @@ cpp! {{{{
 "#
     );
 
-    for [r, c] in BASIC_TYPES.iter() {
+    for (r, c, _) in BASIC_TYPES.iter() {
         println!(
             r#"impl From<&{r}> for Value {{
     fn from(other: &{r}) -> Self {{
--- usd-rs/src/pxr/vt/basic_types.rs
+++ usd-rs/src/pxr/vt/basic_types.rs
@@ -34,3 +34,98 @@ impl AsRef<bool> for Value {
         }
     }
 }
+impl From<&u8> for Value {
+    fn from(other: &u8) -> Self {
+        unsafe {
+            cpp!([other as "const uint8_t *"] -> Value as "pxr::VtValue" {
+                return pxr::VtValue(*other);
+            })
+        }
+    }
+}
+
+impl AsRef<u8> for Value {
+    fn as_ref(&self) -> &u8 {
+        unsafe {
+            cpp!([self as "const pxr::VtValue *"] -> &u8 as "const uint8_t *" {
+                return &(self->Get<uint8_t>());
+            })
+        }
+    }
+}
+impl From<&i32> for Value {
+    fn from(other: &i32) -> Self {
+        unsafe {
+            cpp!([other as "const int32_t *"] -> Value as "pxr::VtValue" {
+                return pxr::VtValue(*other);
+            })
+        }
+    }
+}
+
+impl AsRef<i32> for Value {
+    fn as_ref(&self) -> &i32 {
+        unsafe {
+            cpp!([self as "const pxr::VtValue *"] -> &i32 as "const int32_t *" {
+                return &(self->Get<int32_t>());
+            })
+        }
+    }
+}
+impl From<&u32> for Value {
+    fn from(other: &u32) -> Self {
+        unsafe {
+            cpp!([other as "const uint32_t *"] -> Value as "pxr::VtValue" {
+                return pxr::VtValue(*other);
+            })
+        }
+    }
+}
+
+impl AsRef<u32> for Value {
+    fn as_ref(&self) -> &u32 {
+        unsafe {
+            cpp!([self as "const pxr::VtValue *"] -> &u32 as "const uint32_t *" {
+                return &(self->Get<uint32_t>());
+            })
+        }
+    }
+}
+impl From<&i64> for Value {
+    fn from(other: &i64) -> Self {
+        unsafe {
+            cpp!([other as "const int64_t *"] -> Value as "pxr::VtValue" {
+                return pxr::VtValue(*other);
+            })
+        }
+    }
+}
+
+impl AsRef<i64> for Value {
+    fn as_ref(&self) -> &i64 {
+        unsafe {
+            cpp!([self as "const pxr::VtValue *"] -> &i64 as "const int64_t *" {
+                return &(self->Get<int64_t>());
+            })
+        }
+    }
+}
+impl From<&u64> for Value {
+    fn from(other: &u64) -> Self {
+        unsafe {
+            cpp!([other as "const uint64_t *"] -> Value as "pxr::VtValue" {
+                return pxr::VtValue(*other);
+            })
+        }
+    }
+}
+
+impl AsRef<u64> for Value {
+    fn as_ref(&self) -> &u64 {
+        unsafe {
+            cpp!([self as "const pxr::VtValue *"] -> &u64 as "const uint64_t *" {
+                return &(self->Get<uint64_t>());
+            })
+        }
+    }
+}
