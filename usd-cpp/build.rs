66e7eb87a83380730e7e09c6330d239720c2b75b
Luke Titley <luke@titley.com>
2020-11-15 16:00:32 +0100

    Managed to get the system building for docs only, without downloading third party libraries when building docs

--- usd-cpp/build.rs
+++ usd-cpp/build.rs
@@ -1,6 +1,37 @@
 use std::io::prelude::*;
 use std::process::Command;
 
+fn copy_headers(out_dir: &std::path::PathBuf) -> [std::path::PathBuf; 3] {
+    // The script directory
+    let mut source_include_dir =
+        std::path::PathBuf::from(std::env::current_dir().unwrap());
+    source_include_dir.push("thirdparty");
+    source_include_dir.push("docs");
+    source_include_dir.push("include");
+
+    // The result directory
+    let mut cpp_out_dir = std::path::PathBuf::new();
+    cpp_out_dir.push(&out_dir);
+    cpp_out_dir.push("install");
+
+    // The include directory
+    let mut include_dir = cpp_out_dir.clone();
+    include_dir.push("include");
+
+    std::fs::create_dir_all(cpp_out_dir.clone());
+    let mut options = fs_extra::dir::CopyOptions::default();
+    //options.copy_inside = false;
+    options.overwrite = true;
+    println!("Copying {:?} to {:?}", source_include_dir, include_dir);
+    fs_extra::dir::copy(source_include_dir, cpp_out_dir.clone(),
+                        &options).unwrap();
+
+    let lib = std::path::PathBuf::from("");
+    let lib_dir = std::path::PathBuf::from("");
+
+    [include_dir, lib_dir, lib]
+}
+
 fn build_cpp_usd(out_dir: &std::path::PathBuf) -> [std::path::PathBuf; 3] {
     // The script directory
     let mut script_dir = std::path::PathBuf::from(std::env::current_dir().unwrap());
@@ -86,26 +117,29 @@ fn write_stub_lib_info(out_dir: &std::path::PathBuf) {
 
     write!(
         std::fs::File::create(locations_path).unwrap(),
-        "",
-    );
+        "\
+pub const INCLUDE : &str = \"\"; \n\
+pub const LIBS : &str = \"\"; \n\
+pub const LIB : &str = \"\"; \n\
+");
 }
 
 fn main() {
     // The out directory of the build
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
 
-    if let Ok(_) = std::env::var("DOCS_RS") {
-        // Only run this build job if the USD source directory has changed
-        let mut thirdparty_usd = std::path::PathBuf::from("thirdparty");
-        thirdparty_usd.push("USD");
-        println!(
-            "cargo:rerun-if-changed={}",
-            thirdparty_usd.to_str().unwrap()
-        );
+    // Only run this build job if the USD source directory has changed
+    let mut thirdparty_usd = std::path::PathBuf::from("thirdparty");
+    thirdparty_usd.push("USD");
+    println!(
+        "cargo:rerun-if-changed={}",
+        thirdparty_usd.to_str().unwrap()
+    );
 
+    if let Ok(_) = std::env::var("DOCS_RS") {
+        write_lib_info(&out_dir, copy_headers(&out_dir));
+    } else {
         // Build the usd cpp library
         write_lib_info(&out_dir, build_cpp_usd(&out_dir));
-    } else {
-        write_stub_lib_info(&out_dir);
     }
 }
--- usd-rs/build.rs
+++ usd-rs/build.rs
@@ -1,8 +1,10 @@
 fn main() {
-    // Explicitly link to the usd cpp library. This should propagate upwards
-    // to other libraries
-    println!("cargo:rustc-link-lib={}", usd_cpp::LIB);
-    println!("cargo:rustc-link-search={}", usd_cpp::LIBS);
+    if let Err(_) = std::env::var("DOCS_RS") {
+        // Explicitly link to the usd cpp library. This should propagate upwards
+        // to other libraries
+        println!("cargo:rustc-link-lib={}", usd_cpp::LIB);
+        println!("cargo:rustc-link-search={}", usd_cpp::LIBS);
+    }
 
     // Handle the embedded c++ code
     cpp_build::Config::new()
