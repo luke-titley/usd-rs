8d2abbb608c7dcb9b1259f8d3960b8e8744aa023
Luke Titley <luke@titley.com>
2020-11-16 22:41:48 +0100

    Include the thirdparty headers files in this repo, as we have more space than the usd-cpp repo

--- usd-rs/build.rs
+++ usd-rs/build.rs
@@ -1,14 +1,55 @@
+
+
+fn copy_headers(out_dir: &std::path::PathBuf) -> std::path::PathBuf {
+    // The script directory
+    let mut source_include_dir =
+        std::path::PathBuf::from(std::env::current_dir().unwrap());
+    source_include_dir.push("thirdparty");
+    source_include_dir.push("docs");
+    source_include_dir.push("include");
+
+    // The result directory
+    let mut cpp_out_dir = std::path::PathBuf::new();
+    cpp_out_dir.push(&out_dir);
+    cpp_out_dir.push("install");
+
+    // The include directory
+    let mut include_dir = cpp_out_dir.clone();
+    include_dir.push("include");
+
+    std::fs::create_dir_all(cpp_out_dir.clone());
+    let mut options = fs_extra::dir::CopyOptions::default();
+    options.overwrite = true;
+    fs_extra::dir::copy(source_include_dir, cpp_out_dir.clone(),
+                        &options).unwrap();
+
+    let lib = std::path::PathBuf::from("");
+    let lib_dir = std::path::PathBuf::from("");
+
+    include_dir
+}
+
 fn main() {
-    if let Err(_) = std::env::var("DOCS_RS") {
+    if let Ok(_) = std::env::var("DOCS_RS") {
+        // The out directory of the build
+        let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+        let include_dir = copy_headers(&out_dir);
+
+        cpp_build::Config::new()
+            .include(include_dir)
+            .flag("-std=c++14")
+            .build("src/lib.rs");
+
+    } else {
         // Explicitly link to the usd cpp library. This should propagate upwards
         // to other libraries
         println!("cargo:rustc-link-lib={}", usd_cpp::LIB);
         println!("cargo:rustc-link-search={}", usd_cpp::LIBS);
-    }
 
-    // Handle the embedded c++ code
-    cpp_build::Config::new()
-        .include(usd_cpp::INCLUDE)
-        .flag("-std=c++14")
-        .build("src/lib.rs");
+        // Handle the embedded c++ code
+        cpp_build::Config::new()
+            .include(usd_cpp::INCLUDE)
+            .flag("-std=c++14")
+            .build("src/lib.rs");
+    }
 }
