e7d607f9c084664c9ae9942f9ab4333d27d40f2e
Luke Titley <luke@titley.com>
2020-11-20 00:30:40 +0100

    Added methods to add and remove sub layer paths in a layer. This is how file level referencing happens

--- usd-rs/src/pxr/sdf/layer_handle.rs
+++ usd-rs/src/pxr/sdf/layer_handle.rs
@@ -14,6 +14,33 @@ cpp! {{
 cpp_class!(pub unsafe struct LayerHandle as "pxr::SdfLayerHandle");
 
 impl LayerHandle {
+
+    pub fn insert_sub_layer_path(& mut self, path : &std::ffi::CStr,
+            index : Option<usize>) {
+
+        let path = path.as_ptr() as *const std::os::raw::c_char;
+        let index = if let Some(index) = index { index as i32 } else { -1_i32 };
+
+        unsafe {
+            cpp!([self as "pxr::SdfLayerHandle *",
+                    path as "const char *",
+                    index as "int"]{
+                (*self)->InsertSubLayerPath(std::string(path), index);
+            })
+        };
+    }
+
+    pub fn remove_sub_layer_path(& mut self, index : usize) {
+        let index = index as i32;
+
+        unsafe {
+            cpp!([self as "pxr::SdfLayerHandle *",
+                    index as "int"]{
+                (*self)->RemoveSubLayerPath(index);
+            })
+        };
+    }
+
     pub fn save(&self, force: Option<bool>) -> bool {
         match force {
             Some(force) => unsafe {
