a1acc2f175fa85636a41ff0bedbe9bd9cd760052
Luke Titley <from+badja@luketitley.com>
2023-02-01 12:31:52 +0100

    cargo test works with the docker image

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker run -v $PWD:$PWD --workdir $PWD luketitley/vfxrs_env_usd env USD_ROOT=/usr/local/USD cargo test
+docker run -v $PWD:$PWD --workdir $PWD luketitley/vfxrs_env_usd env USD_ROOT=/usr/local/USD LD_LIBRARY_PATH=/usr/local/USD/lib  cargo test
