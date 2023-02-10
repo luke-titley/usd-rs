b08dc9e98c096c024c811fae5ab4925720602ad6
Luke Titley <from+badja@luketitley.com>
2023-02-10 10:29:01 +0100

    Make xform cache heap allocated

--- usd-rs/src/pxr/usd_geom/xform_cache.rs
+++ usd-rs/src/pxr/usd_geom/xform_cache.rs
@@ -12,19 +12,19 @@ cpp! {{
 }}
 
 //------------------------------------------------------------------------------
-cpp_class!(pub unsafe struct XformCache as "pxr::UsdGeomXformCache"); // TODO LT: This type should be dyanmically allocated like AssetPath.
+/// This is a reference to the underlying UsdXformCache
+///
+#[repr(C, align(8))]
+pub struct XformCacheRef {
+    // A private member stops users from being able to construct it directly
+    _priv: u8,
+}
 
-impl XformCache {
-    pub fn new(time: TimeCode) -> XformCache {
-        //timecode ?
-        unsafe {
-            cpp!([time as "pxr::UsdTimeCode"]
-                        -> XformCache as "pxr::UsdGeomXformCache" {
-                return pxr::UsdGeomXformCache(time);
-            })
-        }
-    }
+// Handy alias to reduce copy/paste errors
+type RefType = XformCacheRef;
 
+//------------------------------------------------------------------------------
+impl XformCacheRef {
     pub fn get_local_to_world_transform(&self, prim: &Prim) -> vt::Matrix4d {
         unsafe {
             cpp!([self as "pxr::UsdGeomXformCache*",
@@ -33,5 +33,48 @@ impl XformCache {
                 return self->GetLocalToWorldTransform(*prim);
             })
         }
+    }
+}
+
+//------------------------------------------------------------------------------
+#[repr(C, align(8))]
+pub struct XformCache {
+    reference: *mut RefType,
+}
+
+//------------------------------------------------------------------------------
+impl XformCache {
+    pub fn new(time: TimeCode) -> XformCache {
+        unsafe {
+            cpp!([time as "pxr::UsdTimeCode"]
+                        -> XformCache as "pxr::UsdGeomXformCache *" {
+                return new pxr::UsdGeomXformCache(time);
+            })
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+impl Drop for XformCache {
+    fn drop(&mut self) {
+        let reference = self.reference.clone();
+        unsafe {
+            cpp!([reference as "const pxr::UsdGeomXformCache*"] {
+                delete reference;
+            })
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+impl AsRef<RefType> for XformCache {
+    fn as_ref(&self) -> &RefType {
+        unsafe { &*(self.reference) }
+    }
+}
+
+impl AsMut<RefType> for XformCache {
+    fn as_mut(&mut self) -> &mut RefType {
+        unsafe { &mut *self.reference }
     }
 }
