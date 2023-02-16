067ffa2401b657fa75ade472f7ebee651b9498ca
Luke Titley <from+badja@luketitley.com>
2023-02-16 15:51:12 +0100

    Added docs for ValueTypeName

--- usd-rs/src/pxr/sdf/value_type_name.rs
+++ usd-rs/src/pxr/sdf/value_type_name.rs
@@ -14,6 +14,24 @@ cpp! {{
 
 //------------------------------------------------------------------------------
 cpp_class!(
+    /// Represents a value type name, i.e. an attribute's type name.  Usually,
+    /// a value type name associates a string with a tf.Type and an optional
+    /// role, along with additional metadata.  A schema registers all known
+    /// value type names and may register multiple names for the same TfType
+    /// and role pair.  All name strings for a given pair are collectively
+    /// called its aliases.
+    ///
+    /// A value type name may also represent just a name string, without a
+    /// tf.Type, role or other metadata.  This is currently used exclusively
+    /// to unserialize and re-serialize an attribute's type name where that
+    /// name is not known to the schema.
+    ///
+    /// Because value type names can have aliases and those aliases may change
+    /// in the future, clients should avoid using the value type name's string
+    /// representation except to report human readable messages and when
+    /// serializing.  Clients can look up a value type name by string using
+    /// sdf.SchemaBase::FindType() and shouldn't otherwise need the string.
+    /// Aliases compare equal, even if registered by different schemas.
     pub unsafe struct ValueTypeName as "pxr::SdfValueTypeName"
 );
 
