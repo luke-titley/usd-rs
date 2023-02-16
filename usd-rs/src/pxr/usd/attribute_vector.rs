c7d4fae47f307f194a4c56ce1a619530777ef716
Luke Titley <from+badja@luketitley.com>
2023-02-16 10:38:08 +0100

    Added an example of how you can use an attribute vector

--- usd-rs/src/pxr/usd/attribute_vector.rs
+++ usd-rs/src/pxr/usd/attribute_vector.rs
@@ -58,6 +58,12 @@ impl std::ops::Index<usize> for AttributeVectorRef {
 
 //------------------------------------------------------------------------------
 /// A contiguous block of [Attribute]s
+///
+/// ```ignore
+/// for attr in prim.get_attributes.iter() {
+///     println!("{}", attr.get_name()?)
+/// }
+/// ```
 #[repr(C, align(8))]
 pub struct AttributeVector {
     reference: *mut RefType,
