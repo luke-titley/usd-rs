fc0487772a630ca5e9344765cb192e8bd97a576c
Luke Titley <from+badja@luketitley.com>
2023-02-19 10:14:27 +0100

    First pass at building tbb directly from source

--- usd-cpp/build.rs
+++ usd-cpp/build.rs
@@ -1,21 +1,51 @@
 use std::io::prelude::*;
 use std::process::Command;
 
-fn build_tbb(thirdparty: &std::path::PathBuf) -> std::path::PathBuf {
+fn get_outdir() -> std::path::PathBuf {
+    std::path::PathBuf::from(
+        std::env::var("OUT_DIR").expect("Unable to obtain OUT_DIR env"),
+    )
+}
+
+fn build_tbb(thirdparty: &std::path::PathBuf) {
+    let mut tbb_lib = get_outdir().clone();
+    tbb_lib.push("libtbb.a");
+
     // thirdparty
-    let mut tbb_src = thirdparty.clone();
-    tbb_src.push("oneTBB");
+    let mut tbb_root = thirdparty.clone();
+    tbb_root.push("oneTBB");
+    let mut tbb_src = tbb_root.clone();
+    tbb_src.push("src");
+
+    let success = Command::new("make")
+        .arg("release")
+        .arg("extra_inc=big_iron.inc")
+        .arg("-j12")
+        .arg(format!("tbb_root={}", tbb_root.display()))
+        .arg(format!("tbb_build_prefix=lib"))
+        .current_dir(tbb_src)
+        .status()
+        .expect("Failed to run make for tbb");
+
+    assert!(success.success(), "Unable to build tbb");
+
+    let mut tbb_built_lib = tbb_root.clone();
+    tbb_built_lib.push("build");
+    tbb_built_lib.push("lib_release");
+    tbb_built_lib.push("libtbb.a");
+    std::fs::rename(tbb_built_lib, tbb_lib)
+        .expect("Unable to move tbb lib out");
+
+    println!("cargo:warning=Install tbb to {}", get_outdir().display())
 
     //println!("cargo:rustc-link-lib={}", lib.to_str().unwrap());
     //println!(
     //    "cargo:rustc-link-search=native={}",
     //    lib_dir.to_str().unwrap()
     //);
-
-    tbb_src
 }
 
-fn build_cpp(out_dir: &std::path::PathBuf) -> [std::path::PathBuf; 3] {
+fn build_cpp(_out_dir: &std::path::PathBuf) -> [std::path::PathBuf; 3] {
     // thirdparty
     let mut thirdparty = std::path::PathBuf::from(
         std::env::current_dir().expect("Unable to obtain the current dir"),
@@ -47,7 +77,7 @@ pub const LIB : &str = \"{}\"; \n\
         info[0].to_str().unwrap(),
         info[1].to_str().unwrap(),
         info[2].to_str().unwrap(),
-    );
+    ).unwrap();
 }
 
 fn write_stub_lib_info(out_dir: &std::path::PathBuf) {
@@ -62,7 +92,7 @@ pub const INCLUDE : &str = \"\"; \n\
 pub const LIBS : &str = \"\"; \n\
 pub const LIB : &str = \"\"; \n\
 "
-    );
+    ).unwrap();
 }
 
 fn write_lib_info_from_env(usd_root: &str, out_dir: &std::path::PathBuf) {
@@ -78,7 +108,7 @@ pub const LIBS : &str = \"{0}/lib\"; \n\
 pub const LIB : &str = \"usd_ms\"; \n\
 ",
         usd_root
-    );
+    ).unwrap();
 }
 
 fn main() {
@@ -86,7 +116,7 @@ fn main() {
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
 
     // Only run this build job if the thirdparty folder has changed
-    println!("cargo:rerun-if-changed=thirdparty");
+    //println!("cargo:rerun-if-changed=thirdparty");
 
     if let Ok(_) = std::env::var("DOCS_RS") {
         write_stub_lib_info(&out_dir);
